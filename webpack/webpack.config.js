01856a2df5c1ab8160d36a1f737f72237694b1e2
doug-hahn <dhahn@tamu.edu>
2021-02-01 16:03:40 -0600

    Update webpack.config.js

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -2,9 +2,13 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  mode: 'production',  
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
     publicPath: './dist/',
   },
+  performance: {
+    hints: false,
+  },  
 };
